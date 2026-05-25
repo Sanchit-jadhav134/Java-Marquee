664c562db170cc7da7bd5cf74bec1e3559fb585a
Sanchit-jadhav134 <sanchitjadhav729@gmail.com>
2026-05-25 11:28:34 +0530

    First commit

---
+++ loops/pattern15.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class pattern15 {
+    public static void main(String[] args){
+        int n = 6;
+        for(int i = n;i>=1;i--){
+            char ch = 'A';
+            for(int j = 1;j<=i;j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
